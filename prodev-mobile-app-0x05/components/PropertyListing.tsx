52e8c2b8df0dd9c81d421d0d31a262ab27e5aecb
Darlington <darlingtonuchenna53@gmail.com>
2026-01-25 16:03:31 +0100

    updates

--- prodev-mobile-app-0x05/components/PropertyListing.tsx
+++ prodev-mobile-app-0x05/components/PropertyListing.tsx
@@ -1,6 +1,5 @@
 import { View, Text } from "react-native";
 import PropertyListingCard from "./common/PropertyListingCard";
-import { useState } from "react";
 import { PropertListing, PropertyListingProps } from "../interfaces";
 
 const PropertyListing = ({ listings }: PropertListing) => {
